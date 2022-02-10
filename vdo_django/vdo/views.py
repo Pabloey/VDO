9f187e04bec4d2fa79a82378c3544e524eba167b
Pablo Yocupicio <Pabloey@yahoo.com>
2022-02-10 13:27:30 -0700

    Views for UploadedVideo and User completed

--- vdo_django/vdo/views.py
+++ vdo_django/vdo/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSerializer, UploadedVideoSerializer
+from .models import User, UploadedVideo
 
 # Create your views here.
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UploadedVideoList(generics.ListCreateAPIView):
+    queryset = UploadedVideo.objects.all()
+    serializer_class = UploadedVideoSerializer
+
+
+class UploadedVideoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UploadedVideo.objects.all()
+    serializer_class = UploadedVideoSerializer
