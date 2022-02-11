be507f99d049b7193073f42aded8766b15aa845a
Pablo Yocupicio <Pabloey@yahoo.com>
2022-02-11 14:59:57 -0700

    Ternary to switch page on 'auth' ready

--- vdo_django/client/src/App.js
+++ vdo_django/client/src/App.js
@@ -1,8 +1,31 @@
 import './App.css';
+import { useState } from 'react'
+import Button from '@mui/material/Button'
+import ReactPlayer from 'react-player'
+import LoggedIn from './component/LoggedIn'
+import LoggedOut from './component/LoggedOut'
 
-function App() {
+function App () {
+
+  const [auth, setAuth] = useState(false)
+
+  const authButton = () => {
+    if (!auth) setAuth(true)
+    else setAuth(false)
+  }
+
+
   return (
     <div className="App">
+      <p>hi</p>
+      <Button onClick={() => authButton()} variant="contained">Testing</Button>
+
+      {auth ? <LoggedIn /> : <LoggedOut />}
+      <a href="youtube.com" ><div>
+        <ReactPlayer controls='false' url='https://vimeo.com/76979871' playing="true" />
+      </div>
+      </a>
+      <ReactPlayer controls='false' url='https://vimeo.com/76979871' playing="false" />
     </div>
   );
 }
