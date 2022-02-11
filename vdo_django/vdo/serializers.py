933f55900b32b1af21a0469d250d1ca9f228e97c
Pablo Yocupicio <Pabloey@yahoo.com>
2022-02-11 00:44:22 -0700

    All serializers connected to each other correctly

--- vdo_django/vdo/serializers.py
+++ vdo_django/vdo/serializers.py
@@ -2,24 +2,6 @@ from rest_framework import serializers
 from .models import User, UploadedVideo, Playlist, Comment, PlaylistVideo
 
 
-class UploadedVideoSerializer(serializers.ModelSerializer):
-
-    user = serializers.HyperlinkedRelatedField(
-        view_name="user_detail",
-        read_only=True
-    )
-
-    user_id = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all(),
-        source='user'
-    )
-
-    class Meta:
-        model = UploadedVideo
-        fields = ('id', 'title', 'description', 'date_time',
-                  'video_url', 'user', 'user_id')
-
-
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.HyperlinkedRelatedField(
         view_name="user_detail",
@@ -44,6 +26,28 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ('id', 'user', 'user_id', 'video', 'video_id', 'description',)
+
+
+class UploadedVideoSerializer(serializers.ModelSerializer):
+
+    user = serializers.HyperlinkedRelatedField(
+        view_name="user_detail",
+        read_only=True
+    )
+    comments = CommentSerializer(
+        many=True,
+        read_only=True
+    )
+
+    user_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        source='user'
+    )
+
+    class Meta:
+        model = UploadedVideo
+        fields = ('id', 'title', 'description', 'date_time',
+                  'video_url', 'user', 'user_id', 'comments')
 
 
 class PlaylistSerializer(serializers.HyperlinkedModelSerializer):
