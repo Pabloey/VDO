d1507c8fe47d5cd1f09a38d094b458232d920817
Pablo Yocupicio <Pabloey@yahoo.com>
2022-02-11 18:22:58 -0700

    Values from SignIn form are registered

--- vdo_django/client/src/pages/SignIn.jsx
+++ vdo_django/client/src/pages/SignIn.jsx
@@ -1,11 +1,22 @@
 import TextField from '@mui/material/TextField';
+import {useState } from 'react'
 
 export default function SignIn() {
+  const [account, setAccount] = useState({
+    username: "",
+    password: "",
+  })
+
+  const handleChange = (e) => {
+    setAccount({...account, [e.target.name]: e.target.value})
+    console.log(account)
+  }
+
   return (
     <div>
-      <TextField/>
+      <TextField id="outlined-basic" label="Username" variant="outlined" name="username" value={account.username} onChange={handleChange} />
       <br/>
-      <TextField/>
+      <TextField id="outlined-basic" label="Password" variant="outlined" name="password" value={account.password} onChange={handleChange} />
     </div>
   )
 }
