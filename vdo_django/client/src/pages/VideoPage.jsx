4b4d2904afd35ee6992363e5378f70baa98af1b4
Pablo Yocupicio <Pabloey@yahoo.com>
2022-02-13 00:05:49 -0700

    Mapping through comments in video page

--- vdo_django/client/src/pages/VideoPage.jsx
+++ vdo_django/client/src/pages/VideoPage.jsx
@@ -17,8 +17,17 @@ export default function VideoPage(props) {
     <div>
       <ReactPlayer url={`${videos.video_url}`} />
       <h3>{videos.user}</h3>
-      <h3>{</h3>
-
+      <h3>{videos.title}</h3>
+      <h5>{videos.description}</h5>
+      <div>
+        <h3>Comments</h3>
+        {videos.comments.map((e, i) => (
+          <div>
+            <h5>{e.user}</h5>
+            <p>{e.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
