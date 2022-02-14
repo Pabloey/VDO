5eed29e143cb5d09d18bef931dc0111bd01fd541
Pablo Yocupicio <Pabloey@yahoo.com>
2022-02-13 19:30:15 -0700

    Submit videos is now working

--- client/src/pages/UploadVideo.jsx
+++ client/src/pages/UploadVideo.jsx
@@ -1,31 +1,48 @@
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { SubmitVideo } from "../services/routes";
 
 export default function UploadVideo() {
   const [videoInput, setVideoInput] = useState({
     title: "",
     description: "",
     video_url: "",
-    user_id: "",
+    user_id: JSON.parse(localStorage.getItem("user")).id,
   });
 
   const handleChange = (e) => {
     setVideoInput({ ...videoInput, [e.target.name]: e.target.value });
+    console.log(videoInput);
   };
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await SubmitVideo(videoInput);
+    setVideoInput({
+      title: "",
+      description: "",
+      video_url: "",
+    });
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <TextField id="outlined-basic" label="Title" variant="outlined" name="username" value={videoInput.title} onChange={handleChange} />
+        <TextField id="outlined-basic" label="Title" variant="outlined" name="title" value={videoInput.title} onChange={handleChange} />
         <br />
         <br />
-        <TextField id="outlined-basic" label="Description" variant="outlined" name="password" value={videoInput.description} onChange={handleChange} />
+        <TextField
+          id="outlined-basic"
+          label="Description"
+          variant="outlined"
+          name="description"
+          value={videoInput.description}
+          onChange={handleChange}
+        />
         <br />
         <br />
-        <TextField id="outlined-basic" label="URL" variant="outlined" name="password" value={videoInput.video_url} onChange={handleChange} />
+        <TextField id="outlined-basic" label="URL" variant="outlined" name="video_url" value={videoInput.video_url} onChange={handleChange} />
         <br />
         <br />
         <Button type="submit" variant="contained">
